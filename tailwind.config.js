ce3ade30df3a8c3058c667677f34e93bb526afb6
lucasquareli1 <161093264+lucasquareli1@users.noreply.github.com>
2024-05-05 17:17:09 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors:{
+        'prymary':'#cc0181' 
+      },
+      fontFamily:{
+        'Fonte-poppins': ('Poppins','sans-serif')
+      }
+    },
+  },
+  plugins: [],
+}
